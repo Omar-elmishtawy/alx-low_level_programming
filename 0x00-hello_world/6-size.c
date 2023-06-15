b2f3bf9f2b5d4d0f445419808f180236d66c4c18
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-15 13:17:09 -0700

    dasd

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - declare some variables
+ * Return: Stops the program
+ */
+int main(void)
+{
+char y;
+int x;
+long int z;
+long long int f;
+float m;
+return (0);
+}
