665fb61bcd79a5f17a1b61d9bd3599a154481d5a
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-15 13:09:00 -0700

    asd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - to print a string 
+ * main - to print a string
  * Return: Stops the program
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
