0093f1826fa693a18b50c4fefc79a62386f21959
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-15 13:42:21 -0700

    sad

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - to print a string
+ * Return: Stops the program
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19")	
+return (0);
+}
