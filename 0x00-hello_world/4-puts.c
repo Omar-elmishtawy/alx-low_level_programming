8bd3641c88dd5b5d0517e40228608c0fb5c2e091
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-15 12:55:27 -0700

    dsad

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 /**
- * main to print a string
+ * main -  to print a string
+ * Description: this was made for 
+ * printing a string that was giving by alx task
+ * Return: Stops the program
  */
 int main(void)
 {
