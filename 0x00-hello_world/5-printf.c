9a0558a3dc1e3cb2fff95b62eb898a3a2c0ee4ee
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-15 13:13:19 -0700

    sda

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - to print a string
+ * Return: Stops the program
+ */
+int main(void)
+{
+printf("with proper grammar, but outcome is a piece of art,\n");
+return (0);
+}
