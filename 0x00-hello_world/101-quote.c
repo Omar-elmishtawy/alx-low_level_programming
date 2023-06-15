65cf881b25b6aa2dd294fd00dc1c41cd9b971695
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-15 14:08:12 -0700

    sda

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-char test[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+char test[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 fprintf(stderr, "%c", test);
 return (1);
 }
