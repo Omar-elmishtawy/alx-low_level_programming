7ca780af357796a457d622b36f16976db88a8fb2
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-16 05:58:14 -0700

    dsa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - get last digit of a number.
+ * Description: this get the last digit
+ * and checks if it greater than five
+ * or less than six.
+ * Return: Stops the program
+ */
+int main(void)
+{
+	int let;
+
+	for (let = 48; let <= 57; let++)
+		putchar(let);
+	for (let = 97; let <= 102; let++)
+		putchar(let);
+	putchar('\n');
+	return (0);
+}
