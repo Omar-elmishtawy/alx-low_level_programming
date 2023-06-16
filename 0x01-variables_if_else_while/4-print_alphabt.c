59df61a9cf1fb2faadeb3c730ce28ca8370482d0
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-16 05:46:06 -0700

    sadasd

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,7 +14,7 @@ int main(void)
 	char let;
 
 	for (let = 'a'; let <= 'z' ; let++)
-		if (let != 'q' || let != 'e')
+		if (let != 'q' && let != 'e')
 			putchar(let);
 	putchar('\n');
 	return (0);
