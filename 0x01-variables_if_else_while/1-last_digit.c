05b6392c16f488b55a6ee96dcb7536e4c246e596
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-16 05:00:30 -0700

    da

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,19 +5,19 @@
 /**
  * main - get last digit of a number.
  * Description: this get the last digit
- * and checks if 
+ * and checks if it greater than five
+ * or less than six.
  * Return: Stops the program
  */
 int main(void)
 {
 	int n;
 	int last_digit;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
-	
-	if (n < 0)
-		last_digit = -1 * last_digit;
+
 	if (last_digit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	else if (last_digit < 6 && last_digit != 0)
