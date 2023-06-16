b4b38342bf765d158e718748ed00b56137ce1861
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-16 05:41:45 -0700

    dsa

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - get last digit of a number.
+ * Description: this get the last digit
+ * and checks if it greater than five
+ * or less than six.
+ * Return: Stops the program
+ */
+int main(void)
+{
+	char let;
+
+	for (let = 'a'; let <= 'z' ; let++)
+		putchar(let);
+	for (let = 'A'; let <= 'Z'; let++)
+		putchar(let);
+	putchar('\n');
+	return (0);
+}
