dd493e1735a3e1eb5181af686fbc92391a51967a
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-16 05:49:38 -0700

    dsa

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - get last digit of a number.
+ * Description: this get the last digit
+ * and checks if it greater than five
+ * or less than six.
+ * Return: Stops the program
+ */
+int main(void)
+{
+	int let;
+
+	for (let = 0; let < 10; let++)
+		prnitf("%d", let);
+	putchar('\n');
+	return (0);
+}
