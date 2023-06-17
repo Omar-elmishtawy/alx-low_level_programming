d0c8ff1b376960574e6ae88a5d7cb590622257f8
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-17 09:22:56 -0700

    dsad

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,17 +14,22 @@ int main(void)
 	int x;
 	int y;
 	int z;
+	int m;
 
-	for (x = 48; x < 56; x++)
+	for (x = 48; x <= 56; x++)
 	{
-		for (y = (x + 1); y < 57; y++)
+		for (y = 48; y <= 57 ; y++)
 		{
-			for (z = (y + 1); z <= 57 ; z++)
+			for (z = 48; z <= 57 ; z++)
 			{
-				putchar(x);
-				putchar(y);
-				putchar(z);
-				if (x < 55)
+				for (m = 48; m <= 57; m++)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(32);
+					putchar(z);
+					putchar(m);
+				if (x <= 57)
 				{
 					putchar(44);
 					putchar(32);
