889bf95bf9e6cc426892f5cda51c2a8fed34f7c3
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-20 14:21:00 -0700

    dlkas

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - this function to print _putchar
+ * Description: we prin using _putchar function
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c<= 122)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
