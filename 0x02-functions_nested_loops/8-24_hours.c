c87d582814692e21ad56cab35bf7b667e15bde85
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-20 15:51:13 -0700

    dasd

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,7 +14,7 @@ void jack_bauer(void)
 
 	for (x = 0; x < 3; x++)
 	{
-		for (y = 0; y < 4; y++)
+		for (y = 0; y < 10; y++)
 		{
 			for (z = 0; z < 6; z++ )
 			{
@@ -26,6 +26,8 @@ void jack_bauer(void)
 					_putchar('0' + z);
 					_putchar('0' + w);
 					_putchar('\n');
+					if (x == 2 && y == 3 && z == 5 && w == 9)
+						break;
 				
 				}
 			
