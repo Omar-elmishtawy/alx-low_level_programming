e8cc0b559a1c1f853df2367192867ac86edfcf3f
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-20 14:30:04 -0700

    dasd

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - this function to print _putchar
+ * Description: we prin using _putchar function
+ * @c: chracter to check in lower case or no
+ * Return: 1 when it is lower case and 0 when otherwise
+ */
+int _islower(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
