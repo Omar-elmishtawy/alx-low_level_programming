edd60ddb476dfd4583dcc170293331b758d41974
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-20 16:26:30 -0700

    das

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * times_table - this function to print _putchar
+ * Description: we prin using _putchar function
+ * Return: 1 when it is lower case and 0 when otherwise
+ */
+void times_table(void)
+{
+	int x;
+	int z;
+	int f;
+	int y = 0;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (z = 0; z <=9; z++)
+		{
+			f = x * y;
+			_putchar('0' + f);
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar('\n');
+		y++;
+	}
+}
