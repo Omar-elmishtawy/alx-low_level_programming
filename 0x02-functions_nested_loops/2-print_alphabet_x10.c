4a0a97af9bbdb9328960c4be1f48cba56c86d530
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-20 07:14:34 -0700

    dsa

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,11 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	char j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 97; j < 123; j++)
+		for (j = 'a'; j < 'z'; j++)
 			_putchar(j);
 		_putchar('\n')
 	}
