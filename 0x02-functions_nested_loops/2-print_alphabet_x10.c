b979f0bd9719030f63f06ae5bda0f49eda145c12
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-20 07:07:30 -0700

    sda

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - this function to print _putchar
+ * Description: we prin using _putchar function
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		print_alphabet();
+}
