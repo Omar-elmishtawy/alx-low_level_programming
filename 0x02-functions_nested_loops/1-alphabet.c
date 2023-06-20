83d3f53c441bdf5c71e607d73cc62058b1503e08
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-20 07:00:33 -0700

    das

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - this function to print _putchar
+ * Description: we prin using _putchar function
+ * Return: Stops the program
+ */
+void print_alphabet(void);
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		_putchar(i)
+	_putchar("\n")		
+
+}
