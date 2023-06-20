c99f26dcbc02edba3f9c44a76cb530efaffb20b6
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-20 06:50:28 -0700

    sda

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-main - this function to print _putchar
-Description: we prin using _putchar function
-Return: Stops the program
-*/
+ * main - this function to print _putchar
+ * Description: we prin using _putchar function
+ * Return: Stops the program
+ */
 int main(void)
 {
 	int i;
-	char arr[8] = { '_', 'p', 'u', 't', 'c', 'h', 'a', 'r' };
+	char arr[9] = { '_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\n' };
 
-	for (i = 0; i <= 7; i++)
+	for (i = 0; i < 9; i++)
 		_putchar(arr[i]);
 	return (0);
 }
