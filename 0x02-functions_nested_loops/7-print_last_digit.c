f4eb64a0a86b3a433748e50c70e66b38fbf17a50
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-20 15:17:04 -0700

    dsaad

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,15 @@
 int print_last_digit(int n)
 {	
 	n = n % 10;
+	if (n > 0)
+	{
 	_putchar('0' + n);
+	}
+	else if (n < 0)
+	{
+		n = n  * -1
+		_putchar('-');
+		_putchar('0' + n)
+	}
 	return (n);
 }
