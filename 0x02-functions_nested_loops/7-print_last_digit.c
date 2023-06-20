1e24320a336f9169e9f8292e7b46a2abd876487b
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-20 15:06:52 -0700

    dasds

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - this function to print _putchar
+ * Description: we prin using _putchar function
+ * @n: chracter to check in lower case or no
+ * Return: 1 when it is lower case and 0 when otherwise
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
