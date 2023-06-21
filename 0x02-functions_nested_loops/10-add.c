37692dcb75154091494697f81ab963f9e3162428
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-20 17:00:46 -0700

    dasd

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - this function to print _putchar
+ * Description: we prin using _putchar function
+ * @x: chracter to check in lower case or no
+ * @y: character two
+ * Return: 1 when it is lower case and 0 when otherwise
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
