5e29c2750db587b8c3a58a42afd77756d8896ab3
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-20 17:13:36 -0700

    dsa

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,9 +10,31 @@
  */
 void print_to_98(int n)
 {
-	while (n < 98)
+	if(n <= 98)
 	{
-		printf("%d, ", n);
-		n++;
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n < 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if ( > 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			n--;
+		}
+	
 	}
 }
