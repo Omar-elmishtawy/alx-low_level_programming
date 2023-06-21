65b18532d18cfe10ff7cbd51b461bbebe445abaa
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-20 17:22:40 -0700

    dasd

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,13 +4,12 @@
 /**
  * print_to_98 - this function to print _putchar
  * Description: we prin using _putchar function
- * @x: chracter to check in lower case or no
- * @y: character two
+ * @n: chracter to check in lower case or n
  * Return: 1 when it is lower case and 0 when otherwise
  */
 void print_to_98(int n)
 {
-	if(n <= 98)
+	if (n <= 98)
 	{
 		while (n <= 98)
 		{
