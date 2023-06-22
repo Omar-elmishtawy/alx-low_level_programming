586addccf77210b6cc82b161fc7cb94c0f0466b0
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-22 12:49:45 -0700

    das

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - this function to print _putchar
+ * Description: we prin using _putchar function
+ * Return: 1 when it is lower case and 0 when otherwise
+ */
+int main(void)
+{
+        int i;
+
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
+}
