7726a4b9624f4b00b477861d84823327261b65d3
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-22 19:04:20 -0700

    dsa

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,6 @@
 
 /**
  * main - test function
- * Description: test
  * Return: 0 to stop program
  */
 
@@ -12,7 +11,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
+		if (i % 5 == 0 && i % 3 == 0)
 		{
 			printf("FizzBuzz");
 
