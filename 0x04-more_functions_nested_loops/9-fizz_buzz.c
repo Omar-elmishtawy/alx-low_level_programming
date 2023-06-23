cc32c317b13fa510cbc14bd3af46bb69efcf998a
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-22 18:48:37 -0700

    dsa

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "stdio.h"
+
+/**
+ * main - test function
+ * Description: test
+ * Return: jfds
+ */
+
+void main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
+		
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		
+		}
+		else if (i % 3 == 0)
+		{
+		
+			printf("Fizz");
+
+		}
+		else
+			printf("%d", i);
+	
+	
+	}
+	return (0);
+}
