19052f27c387f5258c3b0d6794746e52320755a5
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-22 19:13:37 -0700

    das

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,15 +12,15 @@ void print_line(int n)
 	int x;
 	int j;
 
-	for (x = 1; x <= size; x++)
+	for (x = 1; x <= n; x++)
 	{
-		for (j = 1; j <= size; j++)
+		for (j = 1; j <= n; j++)
 		{
 			_putchar('_');
 		}
 		_putchar('\n');
 	}
-	if (size <= 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
