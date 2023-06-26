9675d5cd29378b92ea73b075cafb7386d81705d7
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-26 13:34:18 -0700

    xas

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - test function
+ * Description: test
+ * @str: sizei
+ * Return: jfds
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str)
+		str++;
+	}
+	_putchar('\n')
+}
+
