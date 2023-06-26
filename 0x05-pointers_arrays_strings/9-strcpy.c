bf431050117ea6c73bcac0215dc536e6f825b670
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-26 16:12:09 -0700

    xz

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,13 +11,13 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	dest = src;
+	int i = 0;
 	
 	while(*src != '\0')
 	{
-		*dest = *src;
-		src++;
-		dest++;
+		dest[i] = *src;
+		src;
+		i++;
 	}
 	return (dest);
 }
