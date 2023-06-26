0d179eec95ca16914ab4fc9b85b8b0eff8d9de1c
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-26 15:08:48 -0700

    xzx

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - test function
+ * Description: test
+ * @str: size
+ * Return: jfds
+ */
+
+void puts_half(char *str);
+{
+	int count = 0;
+	int half;
+
+	while (*s)
+	{
+		count++;
+		s++;
+	}
+	half = count / 2;
+	while ( half > 0)
+	{
+		_putchar(*(s - half));
+		half--;
+	}
+}
+
