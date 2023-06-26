04c6970c8a52b1dd0110c8fff37d8cbaa230165a
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-26 13:17:29 -0700

    0dad

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * swap_int(int *a, int *b) - test function
+ * Description: test
+ * @n: size
+ * Return: jfds
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
