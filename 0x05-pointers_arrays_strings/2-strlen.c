65228c55c1427a623a92faaf1035895b3759e0cb
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-26 13:23:21 -0700

    das

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * _strlen - test function
+ * Description: test
+ * @a: size
+ * @b: test
+ * Return: jfds
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+	
+	while(*s)
+		count++;
+	return (count);
+}
+
