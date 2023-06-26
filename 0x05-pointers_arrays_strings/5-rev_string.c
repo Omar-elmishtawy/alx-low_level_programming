a63dcbc423da83f80472776d8d230e155a828794
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-26 14:04:12 -0700

    xzxz

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - test function
+ * Description: test
+ * @s: size
+ * Return: jfds
+ */
+
+void rev_string(char *s)
+{
+	int count = 0;
+	int temp;
+
+	while (*s)
+	{
+		count++;
+		s++;
+	}
+	while (count != 0)
+	{
+		temp = *s;
+		*s = *(s - count);
+		*(s - count) = temp;
+		s--;
+		count++
+	}
+}
+
