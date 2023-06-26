d2569cb5776c096cdd2cf44c7bfcc1a49d52c221
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-26 13:45:45 -0700

    zxz

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - test function
+ * Description: test
+ * @s: size
+ * Return: jfds
+ */
+
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (*s)
+	{
+		count++;
+		s++;
+	}
+	while (count != 0)
+	{
+		_putchar(*(s - 1));
+		count--;
+		s--;
+	}
+}
+
