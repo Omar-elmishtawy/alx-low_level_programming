2bc7079117233340f5b9df888f3ffc8ad69767cc
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-26 15:19:17 -0700

    zxz

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - test function
+ * Description: test
+ * @a: size
+ * @n: sad
+ * Return: jfds
+ */
+
+void print_array(int *a, int n);
+{
+
+	while (n > 0)
+	{
+		printf("%d, ", *a);
+		a++;
+		n--;
+	}
+	_putchar('\n');
+}
