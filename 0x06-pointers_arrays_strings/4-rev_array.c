ff1b7add4114b2c64560324befd9a35a609d9665
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-29 17:46:27 -0700

    das

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_void - test function
+ * Description: test
+ * @a: size
+ * @n: dasd
+ * Return: jfds
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int i;
+
+	n--;
+	for (i = 0; i <= n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i];
+		a[n - i] = temp;
+	}
+}
