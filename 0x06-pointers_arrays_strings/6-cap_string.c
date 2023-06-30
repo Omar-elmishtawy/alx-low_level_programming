3253ead4752bbb34bf33c3a387b3a39a51734e2b
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-29 19:26:43 -0700

    xzxz

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,21 +18,34 @@ char *cap_string(char *a)
 		{
 			if (a[i - 1] == ' ' || a[i - 1] == '\t')
 				a[i] = a[i] - 32;
-			else if (a[i - 1] == '\n' || a[i - 1] == ',')
+			if (a[i - 1] == '\n' || a[i - 1] == ',')
 				a[i] = a[i] - 32;
-			else if (a[i - 1] == ';' || a[i - 1] == '.')
+			if (a[i - 1] == ';' || a[i - 1] == '.')
 				a[i] = a[i] - 32;
-			else if (a[i - 1] == '!' || a[i - 1] == '?')
+			if (a[i - 1] == '!' || a[i - 1] == '?')
 				a[i] = a[i] - 32;
-			else if (a[i - 1] == '"' || a[i - 1] == '(')
+			if (a[i - 1] == '"' || a[i - 1] == '(')
 				a[i] = a[i] - 32;
-			else if (a[i - 1] == ')' || a[i - 1] == '{')
+			if (a[i - 1] == ')' || a[i - 1] == '{')
 				a[i] = a[i] - 32;
-			else if (a[i - 1] == '}')
+			if (a[i - 1] == '}')
 				a[i] = a[i] - 32;
 		}
+		if (a[i - 1] == '\t')
+			a[i - 1] = ' ';
 		i++;
 	}
 
 	return (a);
+}
+
+int main(void)
+{
+    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+    char *ptr;
+
+    ptr = cap_string(str);
+    printf("%s", ptr);
+    printf("%s", str);
+    return (0);
 }
