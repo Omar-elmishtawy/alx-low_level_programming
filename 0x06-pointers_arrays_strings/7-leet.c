d8f32e9b28339d7ae7254d9843d88e7e3176a5b4
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-29 19:58:54 -0700

    dzd

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - test function
+ * Description: test
+ * @a: size
+ * Return: jfds
+ */
+
+char *leet(char *)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		if (a[i] == 'a' || a[i] == 'A')
+			a[i] = 4;
+		if (a[i] == 'e' || a[i] == 'E')
+			a[i] == 3;
+		if (a[i] == 'o' || a[i] == 'O')
+			a[i] == 0;
+		if (a[i] == 't' || a[i] == 'T')
+			a[i] = 7;
+		if (a[i] == 'l' || a[i] == 'L')
+			a[i] == 1;
+		}
+		i++;
+	}
+
+	return (a);
+}
