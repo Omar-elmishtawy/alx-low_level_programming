146618836fadf7e494f2fecee03f799fa5db666f
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-06-29 18:55:09 -0700

    dad

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - test function
+ * Description: test
+ * @a: size
+ * Return: jfds
+ */
+
+char *cap_string(char *a)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 97 && a[i] <= 122)
+		{
+			if (a[i - 1] == ' ' || a[i - 1] == '\t')
+				a[i] = a[i] - 32;
+			else if (a[i - 1] == '\n' || a[i - 1] == ',')
+				a[i] = a[i] - 32;
+			else if (a[i - 1] == ';' || a[i - 1] == '.')
+				a[i] = a[i] - 32;
+			else if (a[i - 1] == '!' || a[i - 1] == '?')
+				a[i] = a[i] - 32;
+			else if (a[i - 1] == '"' || a[i - 1] == '(')
+				a[i] = a[i] - 32;
+			else if (a[i - 1] == ')' || a[i - 1] == '{')
+				a[i] = a[i] - 32;
+			else if (a[i - 1] == '}')
+				a[i] = a[i] - 32;
+		}
+		i++;
+	}
+
+	return (a);
+}
