866b48d7b893303d7c99d8522956e70e76facaf2
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-03 03:19:51 -0700

    vxcb

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - test function
+ * Description: test
+ * @dest: size
+ * @src: b
+ * @n: n
+ * Return: jfds
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (s);
+}
