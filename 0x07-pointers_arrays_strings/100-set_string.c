47e05568251cd584f05496d406137fb90b2a83a4
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-03 09:14:25 -0700

    dasd

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - test function
+ * Description: test
+ * @s: size
+ * @to: ad
+ */
+
+void set_string(char **s, char *to)
+{
+	**s = *to;
+}
