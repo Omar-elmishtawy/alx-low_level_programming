a6b6ebcbf5c74dec398aa77f81fa5c73b886eb90
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-03 07:30:47 -0700

    dasda

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -18,7 +18,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (needle[j] == haystack[i + j] && haystack[i + j] != '\0')
+			if (needle[j] == haystack[i + j])
 				continue;
 			break;
 		}
