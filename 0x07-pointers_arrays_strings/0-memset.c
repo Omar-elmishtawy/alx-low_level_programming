d4ef9a6f7a0fd8b4f3f38f6e1cb624ba5ebea0f7
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-03 02:59:55 -0700

    xzx

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - test function
+ * Description: test
+ * @s: size
+ * @b: b
+ * @n: n
+ * Return: jfds
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
