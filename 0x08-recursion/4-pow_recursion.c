6bdf5ba087e1e8972084801c9d1d1acb4c3f8dac
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-05 13:04:26 -0700

    dasdafaf

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - test function
+ * Description: test
+ * @x: size
+ * @y: size2
+ * Return: jfds
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	else
+		return (x * _pow_recursion(x, (y - 1)));
+}
