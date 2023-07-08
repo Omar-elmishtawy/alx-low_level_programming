3c89fd3550e0619d16cad78feaf788be34c85af9
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-08 08:33:11 -0700

    casc

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int notdigit(char c)
+{
+	if (c >= 48 && c <= 57)
+		return (0);
+	else
+		return (1);
+}
+
+
+
+
+
+/**
+ * main - test function
+ * @argc: num
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+		while(i != argc)
+		{
+			if (notdigit(argv[i][0]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				sum = sum + atoi(argv[i]);
+			i++;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
