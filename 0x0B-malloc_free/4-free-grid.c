dd515117a47141cd7eef0c4b9c25295e601d0398
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-10 16:01:23 -0700

    cassa

---
+++ 0x0B-malloc_free/4-free-grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "stdlib.h"
+#include "stdio.h"
+
+/**
+ * alloc_grid - test function
+ * Description: test
+ * @width: string
+ * @height: kjda
+ * Return: jfds
+ */
+void free_grid(int **grid, int height)
+{
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
