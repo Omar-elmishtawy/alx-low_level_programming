c3172abecb96fb489b711959a879d080024e6aa8
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-10 17:09:27 -0700

    casca

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,17 +17,13 @@ int **alloc_grid(int width, int height)
 	int i = 0;
 	int j = 0;
 	int *ptr;
-
-	if (width <= 0 || height <= 0)
+	
+	arr = malloc(sizeof(* int) * height);
+	if (width <= 0 || height <= 0 || arr == NULL)
 		return (NULL);
 
-	len = sizeof(int *) * height + sizeof(int) * width * height;
-	arr = (int **)malloc(len);
-	ptr = (int *)(arr + height);
-	if (arr == NULL)
-		return (NULL);
 	for (i = 0; i < height; i++)
-		arr[i] = (ptr + width * i);
+		arr[i] = malloc(sizeof(int) * width);
 
 
 	for (i = 0; i < height; i++)
