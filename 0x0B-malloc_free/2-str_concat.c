706c802380aeab58211cd7cda308e2a7789b0ff9
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-11 10:21:04 -0700

    dasd

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,9 +22,9 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL && s2 == NULL)
 		return ("");
 	if (s1 == NULL)
-		*s1 = '\0';
+		s1 = '\0';
 	if (s2 == NULL)
-		*s2 = '\0';
+		s2 = '\0';
 	while (s1[count1] != '\0')
 	{
 		count1++;
