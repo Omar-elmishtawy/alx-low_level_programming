a45c655bcc545930f9e172b2aec6107f6d6052af
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-13 18:11:33 -0700

    casc

---
+++ 0x0C-more_malloc_free/3-arrary_range.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * array_range - main
+ * @min: argc
+ * @max : string
+ *
+ * Return: absolute value of @n
+ */
+int *array_range(int min, int max)
+{
+	char *ar;
+	unsigned int i;
+
+	if (min > max)
+		return (NULL);
+	ar = malloc(sizeof(int) * (max - min));
+	if (ar == NULL)
+		return (NULL);
+	for (i = min; i < max; i++)
+		ar[i] = i;
+	return (ar);
+}
