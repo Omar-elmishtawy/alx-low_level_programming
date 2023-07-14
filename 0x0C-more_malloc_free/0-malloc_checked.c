4920be583fe30346f6aa55f354eb54fdaa608c99
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-13 17:27:15 -0700

    cas

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
-#include "stdio.h"
-
+#include <limits.h>
 /**
  * malloc_checkdd - test function
  * Description: test
@@ -12,7 +13,29 @@ void *malloc_checked(unsigned int b)
 {
 	int *arr = malloc(b);
 
-	if (arr == 0)
+	if (arr == NULL)
 		return (98);
-	return (arr);	
+	return (arr);
+}
+
+int main(void)
+{
+    char *c;
+    int *i;
+    float *f;
+    double *d;
+
+    c = malloc_checked(sizeof(char) * 1024);
+    printf("%p\n", (void *)c);
+    i = malloc_checked(sizeof(int) * 402);
+    printf("%p\n", (void *)i);
+    f = malloc_checked(sizeof(float) * 100000000);
+    printf("%p\n", (void *)f);
+    d = malloc_checked(INT_MAX);
+    printf("%p\n", (void *)d);
+    free(c);
+    free(i);
+    free(f);
+    free(d);
+    return (0);
 }
