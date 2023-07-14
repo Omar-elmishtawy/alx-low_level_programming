1eff8a2162a59c03bd5c502e2e06d1c966ab5f33
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-13 18:07:04 -0700

    dasdas

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	ar = malloc(nmemb *  size);
-	if (arr == NULL)
+	if (ar == NULL)
 		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
 		ar[i] = '\0';
