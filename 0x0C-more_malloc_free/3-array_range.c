73b97432667a9830be536665816044e54a566cca
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-13 18:30:04 -0700

    csac

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -21,7 +21,7 @@ int *array_range(int min, int max)
 	if (array == NULL)
 		return (NULL);
 
-	for (index = 0; index <= size; index++)
+	for (index = 0; index < size; index++)
 		array[index] = min++;
 
 	return (array);
