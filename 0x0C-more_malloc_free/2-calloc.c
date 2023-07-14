50b65de82cd0638a4231ee402223c14320295fc0
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-13 17:50:39 -0700

    sdsa

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; i < s1_len; i++)
 		ar[i] = s1[i];
-	for (i = 0; i < n; i++)
+	for (i = 0; i < s2_len; i++)
 		ar[s1_len + i] = s2[i];
 	ar[s1_len + s2_len] = '\0';
 
