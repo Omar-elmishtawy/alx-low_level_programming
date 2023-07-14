c046709b5bc1d28aa5e6f52a61f8d6844db89cb9
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-13 17:51:54 -0700

    dasd

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		ar[i] = s1[i];
 	for (i = 0; i < n; i++)
 		ar[s1_len + i] = s2[i];
-	ar[s1_len + s2_len] = '\0';
+	ar[s1_len + n] = '\0';
 
 	return (ar);
 }
