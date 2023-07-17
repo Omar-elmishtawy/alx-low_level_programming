23b32461923387b9af4e60996f055d0c2c55982c
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-17 16:54:17 -0700

    casd

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,9 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+		printf("Name: %s\n", d->name ? d->name : (nill));
+		printf("Age: %f\n", d->age ? d->age : (nill));
+		printf("Owner: %s\n", d->owner ? d->owner : (nill));
 
 	}
 
