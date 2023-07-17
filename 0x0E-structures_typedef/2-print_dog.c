9d2cc9888c97b9511a1cc7fa8526808d594529df
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-17 16:51:10 -0700

    casca

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 
 	}
 
