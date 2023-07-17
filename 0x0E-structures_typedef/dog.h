35fc72f02478045a39ebd35495b79060d0aaed14
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-17 16:18:41 -0700

    xas

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG-H
+/**
+ * struct dog - dog struct
+ * @name: name
+ * @age: agee
+ * @owner: owner
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
