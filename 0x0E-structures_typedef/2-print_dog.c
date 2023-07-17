724071910c8a00379f3378512b274308da54a66b
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-17 16:49:49 -0700

    sdad

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,8 +11,8 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		printf("Name: %s\nAge: %.6f\nOwner: %s\n",d->name, d->age, d->owner);
-		
+		printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
+
 	}
 
 
