05bdbf068386b5217425ea41bc9a29ef10d4b605
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-17 16:31:32 -0700

    csac

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include "dog.h"
 /**
  * init_dog - dogyy
  * @d: ointer to the structure variable
@@ -9,8 +10,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
