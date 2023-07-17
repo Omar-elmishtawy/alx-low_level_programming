067b8a2bc83fcc7576b8edc45e6d446809a8ba21
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-17 16:47:07 -0700

    dasd

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * print_dog - dogyy
+ * @d: ointer to the structure variable
+ * Return: nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\nAge: %.6f\nOwner: %s\n",d->name, d->age, d->owner);
+	
+	
+	
+	}
+
+
+}
