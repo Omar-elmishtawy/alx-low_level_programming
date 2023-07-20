4e3d876cfc9a1dddf61fab63d578b9b638159d3d
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-20 16:50:58 -0700

    dasda

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - mskcas
+ * @n: msakc
+ * Return: ksa
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int  i, sum = 0;
+	va_list ptr;
+	
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ptr, int);
+	}
+	va_end(ptr);
+}
