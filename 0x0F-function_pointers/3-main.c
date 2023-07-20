b3f229a04f5265cd43ea77f74bae06125b4f0122
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-20 06:37:57 -0700

    casca

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,6 +20,8 @@ int main(int argc, char **argv)
 	op = get_op_func(argv[2]);
 	if (!op)
 		printf("Error\n"), exit(99);
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+		printf("Error\n"), exit(100);
 
 	printf("%d\n", op(a, b));
 
