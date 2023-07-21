c821c9d7ce39219363c76a017bb9b8536be8201c
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-20 17:22:23 -0700

    cds

---
+++ 0x10-variadic_functions/2-print_Strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - returns the sum of all its parameters.
+ * @n: amount of the arguments.
+ * @separator: ldsm
+ *
+ * Return: sum of its parameters.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(valist, char *));
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(valist);
+}
+
+int main(void)
+{
+    print_strings(", ", 2, "Jay", "Django");
+    return (0);
+}
