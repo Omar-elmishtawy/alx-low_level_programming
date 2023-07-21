d55a8649e291e3d773f7f91fcc177c628815edf7
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-20 17:13:34 -0700

    dasda

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		separator = " ";
 	va_start(ptr, n);
 	for (i = 0; i < n; i++)
-		printf("%d%s ", va_arg(ptr, int), separator);
+		if (i == (n - 1))
+			printf("%d\n", va_arg(ptr, int));
+		else
+			printf("%d%s ", va_arg(ptr, int), separator);
 
 	va_end(ptr);
 }
