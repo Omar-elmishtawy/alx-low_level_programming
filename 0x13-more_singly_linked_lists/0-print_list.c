d7c74324b63387747f724e2af84a7591ade4a7cd
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-07-31 04:36:53 -0700

    0-print

---
+++ 0x13-more_singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - function
+ * @h: pointer to struct list
+ * Return: size_t
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodes_num = 0;
+
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes_num++;
+	}
+	return (nodes_num);
+}
---
+++ 0x13-more_singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - function
+ * @h: pointer to struct list
+ * Return: size_t
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t nodes_num = 0;
+
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		h = h->next;
+		nodes_num++;
+	}
+	return (nodes_num);
+}
