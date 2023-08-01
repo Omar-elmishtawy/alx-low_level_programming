112298d7a9b802fa5f172cdc779ef4fea477802c
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-08-01 10:30:35 -0700

    cv

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * free_listint - function
+ * free_listint2 - function
  * @head: pointer to struct list
  */
 
-void free_listint(listint_t *head)
+void free_listint2(listint_t **head)
 {
 	listint_t *node = head;
 
