6536607fbc337d5b6001488b68bbb3f950d445f1
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-08-01 16:50:49 -0700

    cacav

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	node->n = n;
 	node->next = NULL;
-	temp = *head;	
+	temp = *head;
 	if (idx == 0)
 	{
 		node->next = *head;
