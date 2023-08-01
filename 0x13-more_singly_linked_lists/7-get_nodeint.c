42d4ce2020491d669f15caf54f682779afa1f222
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-08-01 15:26:37 -0700

    index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - function
+ * @head: pointer to struct list
+ * index: int
+ * Return: listint
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+
+	for (; index > 0 && head; index--)
+	{
+		head = head->next;
+	}
+	if (!head)
+		return (NULL);
+	return (head);
+}
