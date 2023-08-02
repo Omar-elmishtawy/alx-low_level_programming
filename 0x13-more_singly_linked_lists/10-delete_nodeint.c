e29d560c9bfe676228d87bf3a8b457a33af7df53
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-08-02 09:17:46 -0700

    fasgfa

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(temp);
 		return (1);
 	}
-	while (index > 1)
+	while (index > 1 && temp)
 	{
 		temp = temp->next;
 		index--;
