cb826251de6faa0c9ef5fefffb4414a370f51613
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-08-02 02:17:52 -0700

    kdslahdk

--- 0x13-more_singly_linked_lists/main.c
+++ 0x13-more_singly_linked_lists/main.c
@@ -1,7 +1,17 @@
 #include "lists.h"
+ 
 int main(void)
 {
-	free_listint2(NULL);
-	printf("Freed !\n");
+	listint_t *head;
+	listint_t *n;
+
+	head = NULL;
+	n = insert_nodeint_at_index(&head, 0, 98);
+	if (n)
+		printf("-> %d\n", n->n);
+	else
+		printf("(nil)\n");
+	print_listint(head);
 	return (0);
 }
+
