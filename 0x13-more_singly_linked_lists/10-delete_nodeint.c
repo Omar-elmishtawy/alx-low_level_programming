c4507f5a2aa71f5c19c9be92ccae4ef3ae704fd1
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-08-02 09:16:08 -0700

    fasgfa

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,7 +17,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	temp = *head;
 	if (index == 0)
 	{
-		temp = *head;
 		*head = (*head)->next;
 		free(temp);
 		return (1);
@@ -27,10 +26,12 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		temp = temp->next;
 		index--;
 	}
-	temp2 = temp;
-	printf("heeeeeeeeeeeeeey %d\n", ((temp->next)->next)->n);
-	temp->next = (temp->next)->next;
-	(temp2->next)->next = NULL;
-	free(temp2->next);
-	return (1);
+	if (temp)
+	{
+		temp2 = temp->next;
+		temp->next = (temp->next)->next;
+		free(temp2);
+		
+	}
+	return (-1);
 }
