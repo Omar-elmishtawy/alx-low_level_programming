fdfda254b0f341866432ce6ab2c474df46440ef1
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-08-03 17:00:01 -0700

    casca

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,6 +8,7 @@
 unsigned int power(unsigned int x, unsigned int y)
 {
 	unsigned int power = x;
+
 	while (y > 1)
 	{
 		power = power * x;
@@ -38,16 +39,15 @@ unsigned int binary_to_uint(const char *b)
 	result = b[i] - '0';
 	while (i > 0)
 	{
-		if (isdigit(b[count]))
+		if (isdigit(b[count]) && (b[count] == 48 || b[count] == 49))
 			x = b[count] - '0';
 		else
 			return (0);
 		result = result + (x * power(2, i));
 		i--;
 		count++;
-		
+
 	}
-	
 
 	return (result);
 }
