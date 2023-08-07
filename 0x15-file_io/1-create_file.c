c2529cdaccc027e6e6398227c8c7d7bbe21e656c
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-08-07 14:39:53 -0700

    dac ca

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * create_file - create file
+ * @filename: filename
+ * @text_content: sasa
+ * Return: number of printed letter
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT, 0600);
+	if (fd == -1)
+		return (-1);
+
+	write(fd, text_content, strlen(text_content));
+	close(fd);
+	return (1);
+}
