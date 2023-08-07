bcffb54870d7d6b890b8777c14f5d23aea3d9d57
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-08-07 16:43:48 -0700

    gaga

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s cpfile_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: cpfile_from file_to\n");
 		exit(97);
 	}
 	cp_from_to(argv[1], argv[2]);
@@ -30,6 +30,7 @@ void cp_from_to(const char *file_from, const char *file_to)
 	char buf[8 * 1024];
 	int df_from, df_to;
 	size_t bytes;
+	int stat;
 
 	df_from = open(file_from, O_RDONLY);
 	if (!file_from || df_from == -1)
@@ -37,7 +38,7 @@ void cp_from_to(const char *file_from, const char *file_to)
 		dprintf(2, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
-	df_to = open(file_to, O_RDWR | O_CREAT | O_TRUNC, 0660);
+	df_to = open(file_to, O_RDWR | O_CREAT | O_TRUNC | O_APPEND, 0664);
 	if (df_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", file_to);
@@ -48,6 +49,16 @@ void cp_from_to(const char *file_from, const char *file_to)
 		write(df_to, &buf[0], bytes);
 	} while (bytes);
 
-	close(df_from);
-	close(df_to);
+	stat = close(df_from);
+	if (stat == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", df_from);
+		exit(100);
+	}
+	stat = close(df_to);
+	if (stat == -1)
+	{	
+		dprintf(2, "Error: Can't close fd %d\n", df_to);
+		exit(100);
+	}
 }
