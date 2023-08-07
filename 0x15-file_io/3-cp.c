6c28843c3e99d651401ce6ada915a89d1bb3f5ac
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-08-07 15:59:26 -0700

    casca

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+
+
+
+int main(int argc, char **argv)
+{
+	if(argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s cpfile_from file_to\n", argv[0]);
+		exit(97);	
+	}	
+	cp_from_to(argv[1], argv[2]);
+	return(0);
+}
+
+void cp_from_to(const char *file_from, const char *file_to)
+{
+	char buf[8 * 1024];
+	int df_from, df_to;
+	size_t bytes;
+
+	df_from = open(file_from, O_RDONLY);
+	if (!file_from || df_from == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	df_to = open(file_to, O_CREAT | O_TRUNC | O_RDWR, 0664);
+	if (df_to == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+	bytes = read(df_from, &buf[0], 1024);
+	write(df_to, &buf[0], bytes);
+
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,4 +9,5 @@
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
+void cp_from_to(const char *file_from, const char *file_to);
 #endif
