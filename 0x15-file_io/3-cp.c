246ad3e76a88768e1572181877c32faa943ab12e
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-08-08 01:05:30 -0700

    va

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cpfile_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	cp_from_to(argv[1], argv[2]);
@@ -35,34 +35,34 @@ void cp_from_to(const char *file_from, const char *file_to)
 	df_from = open(file_from, O_RDONLY);
 	if (!file_from || df_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 	df_to = open(file_to, O_RDWR | O_CREAT | O_TRUNC | O_APPEND, 0664);
 	if (df_to == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 		exit(99);
 	}
 	do {
 		bytes = read(df_from, buf, 1024);
 		if (write(df_to, buf, bytes) != bytes)
-			dprintf(2, "Error: Can't write to %s\n", file_to), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to), exit(99);
 	} while (bytes);
 
 	if (bytes == -1)
-		dprintf(2, "Error: Can't read from %s\n", file_from), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", file_from), exit(98);
 
 	stat = close(df_from);
 	if (stat == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", df_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", df_from);
 		exit(100);
 	}
 	stat = close(df_to);
 	if (stat == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", df_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", df_to);
 		exit(100);
 	}
 }
