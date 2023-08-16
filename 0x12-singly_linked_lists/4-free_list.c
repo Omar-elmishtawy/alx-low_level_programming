5455f630afc5e2e32da58646c4a7acd9a29bb48d
Omar-elmishtawy <omar.elmishtawy@gmail.com>
2023-08-16 11:18:06 -0700

    vahbh

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,15 +9,11 @@ void free_list(list_t *head)
 {
 	list_t *node = head;
 
-	if (head)
-	{
-		while (head->next)
+		while (head != NULL)
 		{
+			node = head;
 			head = head->next;
 			free(node->str);
 			free(node);
-			node = head;
 		}
-		free(head);
-	}
 }
