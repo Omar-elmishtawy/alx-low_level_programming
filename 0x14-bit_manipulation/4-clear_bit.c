80b60d7656c09202856cc4e7b5ce2b24162e5577
Omar.elmishtawy <omar.elmishtawy@outlook.com>
2023-09-28 11:17:04 -0700

    caca

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * set_bit - as
+ * @n: das
+ * @index: kdas
+ *
+ * Return: 1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int x = 1;
+
+	if (!n || index > sizeof(n) * 8)
+		return (-1);
+
+	x = x << index;
+	*n = (~*n) | x;
+	*n = (~*n);
+
+	return (1);
+
+}
