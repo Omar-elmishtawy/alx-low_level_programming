2c55f02d9c3365993a4de03ad40c902626b70e87
Omar.elmishtawy <omar.elmishtawy@gmail.com>
2023-11-09 10:36:38 -0800

    dasd

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - function
+ * @h: address of head
+ * @idx: index
+ * @n: n
+ * Return: pointer adress of the node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new = NULL;
+	dlistint_t *temp = *h;
+	unsigned int count = 0;
+
+	if (idx == 0)
+	{
+		new = add_dnodeint(h, n);
+		return (new);
+	}
+
+	while (temp->next)
+	{
+		count++;
+		temp = temp->next;
+	}
+	if (idx > count)
+	{
+		return (NULL);
+	}
+	if (idx == count)
+	{
+		new = add_dnodeint_end(h, n);
+		return (new);
+	}
+	temp = *h;
+	while (idx != 1)
+	{
+		temp = temp->next;
+		idx--;
+	}
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->prev = temp;
+	(temp->next)->prev = new;
+	new->next = temp->next;
+	temp->next = new;
+	return (new);
+}
