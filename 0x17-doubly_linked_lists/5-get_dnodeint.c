b2b21f8f87317216d613da95921ae92039d42f87
Omar.elmishtawy <omar.elmishtawy@gmail.com>
2023-11-09 09:45:16 -0800

    das

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -3,13 +3,16 @@
 /**
  * get_dnodeint_at_index - function
  * @head: address of head
- * @index : index
- * Return : pointer adress of the node
+ * @index: index
+ * Return: pointer adress of the node
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
 	dlistint_t *temp = head;
+
+	if (!head)
+		return (NULL);
 
 	while (temp->next)
 	{
