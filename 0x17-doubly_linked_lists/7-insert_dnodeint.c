7e9aecad91933cf9442fe66b0a32cacd2b5b2ecf
Omar.elmishtawy <omar.elmishtawy@gmail.com>
2023-11-09 11:05:28 -0800

    das

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,11 +19,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (new);
 	}
 
-	while (temp->next)
+	while (temp)
 	{
 		count++;
 		temp = temp->next;
 	}
+	printf("%d", count);
 	if (idx > count)
 	{
 		return (NULL);
--- 0x17-doubly_linked_lists/main.c
+++ 0x17-doubly_linked_lists/main.c
@@ -13,6 +13,7 @@ int main(void)
     dlistint_t *head;
 
     head = NULL;
+    add_dnodeint(&head, 5);
     add_dnodeint_end(&head, 0);
     add_dnodeint_end(&head, 1);
     add_dnodeint_end(&head, 2);
@@ -20,10 +21,9 @@ int main(void)
     add_dnodeint_end(&head, 4);
     add_dnodeint_end(&head, 98);
     add_dnodeint_end(&head, 402);
-    add_dnodeint_end(&head, 1024);
     print_dlistint(head);
     printf("-----------------\n");
-    insert_dnodeint_at_index(&head, 5, 4096);
+    insert_dnodeint_at_index(&head, 7, 4096);
     print_dlistint(head);
     free_dlistint(head);
     head = NULL;
