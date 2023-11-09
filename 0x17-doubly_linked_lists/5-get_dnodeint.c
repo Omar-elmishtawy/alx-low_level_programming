ebdd36f69c24e7a30403fb2ca62572685b13cd8d
Omar.elmishtawy <omar.elmishtawy@gmail.com>
2023-11-09 09:41:32 -0800

    test

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - function
+ * @head: address of head
+ * @index : index
+ * Return : pointer adress of the node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+	dlistint_t *temp = head;
+
+	while (temp->next)
+	{
+		count++;
+		temp = temp->next;
+	}
+	if (index > count)
+	{
+		return (NULL);
+	}
+	while (index != 0)
+	{
+		head = head->next;
+		index--;
+	}
+	return (head);
+}
