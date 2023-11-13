ff86c37cdc7b9f03f0ccf6810d9b784f38356c3a
Omar.elmishtawy <omar.elmishtawy@gmail.com>
2023-11-13 05:50:32 -0800

    dsa

---
+++ 0x18-dynamic_libraries/0-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _atoi - this function to print _putchar
+ * Description: we prin using _putchar function
+ * @s: chracter to check in lower case or no
+ * Return: 1 when it is lower case and 0 when otherwise
+ */
+int _atoi(char *s)
+{
+	return (0);
+}
--- 0x18-dynamic_libraries/_putchar.c
+++ 0x18-dynamic_libraries/_putchar.c
@@ -1,16 +1,5 @@
 #include "main.h"
-
-/**
- * main - this function to print _putchar
- * Description: we prin using _putchar function
- * Return: Stops the program
- */
-int main(void)
+int _putchar(char c)
 {
-	int i;
-	char arr[9] = { '_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\n' };
-
-	for (i = 0; i < 9; i++)
-		_putchar(arr[i]);
-	return (0);
+	//empty
 }
