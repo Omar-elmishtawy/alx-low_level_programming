df402db99d0f3efe3c0fd05d08c0c2ca2d6428b4
Omar_elmishtawy <omar.elmishtawy@gmail.com>
2023-11-29 21:52:00 -0800

    return NUll if memory not allocated

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,6 +12,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table->size = size;
 	hash_table->array = malloc(size * sizeof(hash_node_t*));
 
+	if (!hash_table)
+		return NULL;
+
+	if (!array)
+		return NULL;
+
 	for(i = 0; i < size; i++)
 	{
 		hash_table->array[i] = NULL;
