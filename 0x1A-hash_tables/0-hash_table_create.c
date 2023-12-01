76a0d6b3e1bcd785b763337248e0506fb12c876c
Omar_elmishtawy <omar.elmishtawy@gmail.com>
2023-11-30 17:04:45 -0800

    da

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,16 +8,14 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i = 0;
+	hash_table_t *hash_table;
 
-	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
-
+	hash_table = malloc(sizeof(hash_table_t));
 	hash_table->size = size;
-
-	hash_table->array = malloc(size * sizeof(hash_node_t *));
-
 	if (!hash_table)
 		return (NULL);
-
+	
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
 	if (!(hash_table->array))
 	{
 		free(hash_table);
