e3841cd556f85b71717e82b4e513432fee2683fe
Omar_elmishtawy <omar.elmishtawy@gmail.com>
2023-12-01 17:28:40 -0800

    das

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_get - get value
+ * @ht: pointer to hash table
+ * @key: the key
+ * Return: value
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
