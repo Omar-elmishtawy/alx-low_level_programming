81b7ea0b0222d4bff5e275277425a6a128302ece
Omar_elmishtawy <omar.elmishtawy@gmail.com>
2023-12-01 18:00:53 -0800

    ca

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,5 +7,7 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
+	if (ht == NULL)
+		return;
 	free_table(ht);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,4 +40,5 @@ void free_item(hash_node_t *item);
 hash_node_t *create_item(const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 #endif
