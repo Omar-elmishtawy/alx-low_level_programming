00d4cae58259bad81e833c312d08a526bcc56272
root <omar.elmishtawy@gmail.com>
2024-03-12 16:27:34 -0700

    fix

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,5 @@
-#ifndef SEARCH_ALOGS_H
-#define SEARCH_ALGOS_H
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
 #include <stdio.h>
 #include <stdlib.h>
 int linear_search(int *array, size_t size, int value);
